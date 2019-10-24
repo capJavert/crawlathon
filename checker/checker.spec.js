2a66cc3b25fd52402e9c005cc554780791b0ed29
Antonio Martinović <martinovic.antonio@gmail.com>
2019-10-24 18:09:41 +0200

    Check report, fix sorted list of links

--- checker/checker.spec.js
+++ checker/checker.spec.js
@@ -3,7 +3,7 @@ const checker = require('./checker');
 
 const expect = chai.expect;
 
-test('Returns valid report', () => {
+test('Returns valid check results', () => {
   const validResults = [
     'http://acrocat.com/AcroWiki/downloads/AcroWiki_demo.zip',
     'http://activate.imsisoft.com/gennedfreetrial.aspx?productpage=TCP18Pro',
@@ -44,3 +44,19 @@ test('Returns valid report', () => {
   expect(expectedMissing).to.eql(result.missing);
   expect(expectedExtra).to.eql(result.extra);
 });
+
+test('Returns valid report', () => {
+  const foundResults = [
+    'http://acrocat.com/AcroWiki/downloads/AcroWiki_demo.zip',
+    'http://ad-dl.appvv.com/disanfang_flashlight_1043/com.vflashlight.apk',
+    'https://get.downloads32.net/20/193069/132441/CrosswordPuzzleHelper.zip',
+    'https://gigabyte-ga-p61-s3-b3-rev-1-1-easy-tune6-utility-for-intel.soft32.com/file-download/1158329/?nc=',
+    'https://gigabyte-ga-p61-s3-b3-rev-1-1-intel-inf-driver.soft32.com/file-download/1135398/?nc=',
+    'https://nohaunt.soft32.com/file-download/457666/?',
+    'https://noho-tournament-manager.soft32.com/file-download/394232/?nc=',
+    'https://the-mana-world.soft32.com/file-download/840855/?nc=',
+    'https://the-story-of-dragons.soft32.com/file-download/303883/?nc='
+  ]
+
+  checker.report(foundResults);
+})
